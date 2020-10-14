8ddd3afa0f8b1e69b5fbd8b77ea354891828d71a
michellenaim <michellenaim@gmail.com>
2020-10-14 14:09:36 -0700

    Added styling to Movie Nav Bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -4,27 +4,29 @@ import { Link } from 'react-router-dom';
 const NavBar = ({ path, currentUser, logout }) => {
     const display = currentUser ? (
         <div className="navloggedin">
-            <p>Home</p>
-            <p>Genres</p>
-            <p>My List</p>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-                <input type="text" placeholder="Search.." name="search"></input>
-                    <button type="submit"><i class="fa fa-search"></i></button>
-            <p>{currentUser.email}</p>
-            <button className="btn" onClick={logout}>Log Out</button>
+            <div className="nav-left">
+                <Link to="/"><img className="kineto-logo" src={window.kinetoLogo}></img></Link>
+                <p className="nav-title">Home</p>
+                <p className="nav-title">Genres</p>
+                <p className="nav-title">My List</p>
+            </div>
+            <div className="nav-right">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                <button type="submit" className="search-icon"><i className="fa fa-search"></i></button>
+                <p className="email-nav">{currentUser.email}</p>
+                <button className="btn" onClick={logout}>Log Out</button>
+            </div>
         </div>
     ) : (
             <div>
+                <Link to="/"><img className="kineto-logo" src={window.kinetoLogo}></img></Link>
                 {(path !== "/login" && path !== "/signup") && (<Link className="btn" to="/login">Sign In</Link>)}
             </div>
         );
 
     return (
         <header className="nav-bar">
-            <Link to="/"><img className="kineto-logo" src={window.kinetoLogo}></img></Link>
-            <div>
-                {display}
-            </div>
+            {display}
         </header>
     );
 };
