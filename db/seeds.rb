8c662bcc6431695371926d9f30d85207a81798f3
michellenaim <michellenaim@gmail.com>
2020-10-16 16:52:45 -0700

    Create Mute button & add conditional in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
-Movie.destroy_all
-Movie.connection.execute('ALTER SEQUENCE movies_id_seq RESTART WITH 1')
+case Rails.env
+  when "development"
+      Movie.destroy_all
+      User.destroy_all
 
-# user= User.create!()
+      Movie.connection.execute('ALTER SEQUENCE movies_id_seq RESTART WITH 1')
 
-movie1 = Movie.create!(
+      demouser= User.create!(email: "test@test.com", password: "password")
+
+      movie1 = Movie.create!(
         title: "The Silence Of The Lambs",
-        description: "FBI trainee Clarice Starling ventures into a maximum-security asylum to pick the diseased brain of Hannibal Lecter, a psychiatrist turned cannibal.",
-        year: 1991, 
-        rating: "R", 
-        runtime: "1h58",  
-      )
+              description: "FBI trainee Clarice Starling ventures into a maximum-security asylum to pick the diseased brain of Hannibal Lecter, a psychiatrist turned cannibal.",
+              year: 1991, 
+              rating: "R", 
+              runtime: "1h58",  
+            )
+  when "production"
+         Movie.destroy_all
+      User.destroy_all
 
-movie2 = Movie.create!(
+      Movie.connection.execute('ALTER SEQUENCE movies_id_seq RESTART WITH 1')
+
+      demouser= User.create!(email: "test@test.com", password: "password")
+
+      movie1 = Movie.create!(
+        title: "The Silence Of The Lambs",
+              description: "FBI trainee Clarice Starling ventures into a maximum-security asylum to pick the diseased brain of Hannibal Lecter, a psychiatrist turned cannibal.",
+              year: 1991, 
+              rating: "R", 
+              runtime: "1h58",  
+            )
+
+      movie2 = Movie.create!(
         title: "Enola Holmes",
         description: "While searching for her missing mother, intrepid teen Enola Holmes uses her sleuthing skills to outsmart big brother Sherlock and help a runaway lord.",
         year: 2020, 
         rating: "PG 13", 
         runtime: "2h03", 
       )
-
-movie3= Movie.create!(
+      
+      movie3= Movie.create!(
         title: "The Irishman",
         description: "Hit man Frank Sheeran looks back at the secrets he kept as a loyal member of the Bufalino crime family in this acclaimed film from Martin Scorsese.",
         year: 2019, 
@@ -35,3 +55,6 @@ movie3= Movie.create!(
         runtime: "3h29",
       )
 
+      file = open('https://kineto-seeds.s3-us-west-1.amazonaws.com/media/1/movie.mp4')
+      movie1.url.attach(io: file, filename: 'thumbnail.jpg')
+end
--- frontend/components/main_movie/movie_main.jsx
+++ frontend/components/main_movie/movie_main.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import NavBarContainer from '../nav_bar/nav_bar_container';
 
 
 class MovieMain extends React.Component{
     constructor(props) {
         super(props)
+        this.state = {muted: false}
+        this.changeMute = this.changeMute.bind(this)
     }
 
     componentDidMount() {
         this.props.fetchMovies();
     }
 
+    changeMute (e) {
+        e.preventDefault();
+        // debugger
+        // let mainMovie = document.getElementsByClassName("main-movie-video")
+        // mainMovie.muted = !mainMovie.muted;
+        this.setState({ muted: !this.state.muted });
+    }
+
     render() {
         const {movies} = this.props
+        const {muted} = this.state
+
+        const sound = muted ?
+            (<button className="mute" onClick={this.changeMute}>
+                    <i className="fas fa-volume-mute"></i>
+            </button>
+            ) : (
+                <button className="mute" onClick={this.changeMute}>
+                    <i className="fas fa-volume-up"></i>
+                    
+                </button>)
+
         const allMovies = movies.map((movie) => {
             return (
                 <>
@@ -21,12 +42,14 @@ class MovieMain extends React.Component{
                         <p className="main-movie-title">{movie.title}</p>
                         <p className="main-movie-des">{movie.description}</p>
                         <div className="main-buttons">
-                            <button className="main-play-button"><Link to={`/`}><i className="fa fa-play" aria-hidden="true"></i>&nbsp;&nbsp; Play</Link></button>
-                            <button className="main-info-button"><Link to={`/`}><i className="fa fa-info-circle" aria-hidden="true"></i>&nbsp;&nbsp; More Info</Link></button>
+                            <div className="left-buttons">
+                                <button className="main-play-button"><Link to={`/`}><i className="fa fa-play" aria-hidden="true"></i>&nbsp;&nbsp; Play</Link></button>
+                                <button className="main-info-button"><Link to={`/`}><i className="fa fa-info-circle" aria-hidden="true"></i>&nbsp;&nbsp; More Info</Link></button>
+                            </div>
                         </div>
                     </div>
                     {/* <img src={movie.photoURL}/> */}
-                    <video className="main-movie-video" muted="true" autoPlay height="595" loop key={movie.movieURL}>
+                    <video className="main-movie-video" muted={this.state.muted} autoPlay height="595" loop key={movie.movieURL}>
                         <source src={movie.movieURL} type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
@@ -38,6 +61,7 @@ class MovieMain extends React.Component{
 
         return (
             <div className="main-video-container">
+                {sound}
                 {movie}
             </div>
         )
