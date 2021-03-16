6541139f17a0d0b90ebbbe2c8c776c8f161bf8ef
michellenaim <michellenaim@gmail.com>
2021-03-16 12:41:31 -0700

    Fix genre loading bug

--- frontend/components/genres/genre.jsx
+++ frontend/components/genres/genre.jsx
@@ -1,80 +1,89 @@
-import React from "react";
-import GenreItem from "./genre_item"
+import React from 'react';
+import GenreItem from './genre_item';
 
 class Genre extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       preview: null,
-      left: 0
+      left: 0,
     };
-    this.slider = React.createRef()
-  };
+    this.slider = React.createRef();
+  }
 
   componentDidMount() {
     this.props.fetchMoviesGenres();
     this.props.fetchLists();
-    this.props.fetchGenre(this.props.genreId);
+    // this.props.fetchGenre(this.props.genreId);
   }
 
-  componentDidUpdate(prevProps){
-    if (this.props.genreId !== prevProps.genreId && this.props.genreId !== "users"){
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.genreId !== prevProps.genreId &&
+      this.props.genreId !== 'users'
+    ) {
       this.props.fetchGenre(this.props.genreId);
     }
   }
 
-   scroll(direction){
-        let offset;
-        direction === 'left' ? offset = -800 : offset = 800;
-        let width = this.slider.current.scrollWidth;
-        let newPosition = this.slider.current.scrollLeft + offset ;
-        let overflow = newPosition === width;
-        if(overflow) this.slider.current.scrollTo(0,0);
-        if((newPosition < 8 )&&(direction === 'left')) this.slider.current.scrollTo(width,0);
-          this.slider.current.scrollBy(offset ,0)  
-    }
+  scroll(direction) {
+    let offset;
+    direction === 'left' ? (offset = -800) : (offset = 800);
+    let width = this.slider.current.scrollWidth;
+    let newPosition = this.slider.current.scrollLeft + offset;
+    let overflow = newPosition === width;
+    if (overflow) this.slider.current.scrollTo(0, 0);
+    if (newPosition < 8 && direction === 'left')
+      this.slider.current.scrollTo(width, 0);
+    this.slider.current.scrollBy(offset, 0);
+  }
 
   render() {
-
     if (!this.props.genre || this.props.movies.length < 1) {
       return null;
     }
 
-    if (this.props.moviesgenres && Object.values(this.props.moviesgenres).length < 1) {
+    if (
+      this.props.moviesgenres &&
+      Object.values(this.props.moviesgenres).length < 1
+    ) {
       return null;
     }
-    
+
     const { genre, movies } = this.props;
 
     // const genreMovies = movies.filter((movie) =>
     //   new Set(genre.movieIds).has(movie.id)
     // );
-    const rowLen = movies.length
+    const rowLen = movies.length;
     return (
       <div className="genre">
         <p className="genre-title">{genre.name}</p>
-        <div className="btn prev" onClick={() => this.scroll('left')}>{"<"}</div>
-          <div className="movies" ref={this.slider}>
-            {movies.map((movie, i) => {
-                return(
-                  <div>
-                      <GenreItem
-                      key={movie.id}
-                      movie={movie}
-                      selected={this.state.preview === movie.id}
-                      onMouseEnter={() => this.setState({ preview: movie.id })}
-                      onMouseLeave={() => this.setState({ preview: null })}
-                      deleteListMovie={this.props.deleteListMovie}
-                      addToList={this.props.addToList}
-                      currentUser={this.props.currentUser}
-                      lists={this.props.lists}
-                    />
-                  </div>
-                )
-              }
-            )}
-          </div>
-         <div className="btn next" onClick={() => this.scroll('right')}>{">"}</div> 
+        <div className="btn prev" onClick={() => this.scroll('left')}>
+          {'<'}
+        </div>
+        <div className="movies" ref={this.slider}>
+          {movies.map((movie, i) => {
+            return (
+              <div>
+                <GenreItem
+                  key={movie.id}
+                  movie={movie}
+                  selected={this.state.preview === movie.id}
+                  onMouseEnter={() => this.setState({ preview: movie.id })}
+                  onMouseLeave={() => this.setState({ preview: null })}
+                  deleteListMovie={this.props.deleteListMovie}
+                  addToList={this.props.addToList}
+                  currentUser={this.props.currentUser}
+                  lists={this.props.lists}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="btn next" onClick={() => this.scroll('right')}>
+          {'>'}
+        </div>
       </div>
     );
   }
--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,97 +1,88 @@
-import React from "react";
+import React from 'react';
 // import SearchVideoList from './search_video_list';
-import GenreItem from "../genres/genre_item"
+import GenreItem from '../genres/genre_item';
 
 class Search extends React.Component {
-  constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.debounceTimer = undefined;
-        this.debounce = this.debounce.bind(this);
-        
-        this.state = {
-          preview: null,
-          left: 0
-        };
+    this.debounceTimer = undefined;
+    this.debounce = this.debounce.bind(this);
+
+    this.state = {
+      preview: null,
+      left: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchMovies();
+  }
+
+  componentDidUpdate(prevState) {
+    const query = this.props.query;
+    if (prevState.query != query) {
+      this.debounce(() => {
+        if (query.length > 0) this.props.searchByName(query);
+      }, 2000)();
     }
+  }
 
-    componentDidMount(){
-      this.props.fetchMovies()
+  debounce(func, delay) {
+    return () => {
+      if (this.debounceTimer) clearTimeout(this.debounceTimer);
+      this.debounceTimer = setTimeout(func, delay);
+    };
+  }
+
+  showSearch(movies, elementToSkip = 6) {
+    if (Object.values(movies).length == 0) {
+      return <></>;
     }
+    return (
+      <>
+        <div className="genre">
+          <div className="search-items">
+            {movies.map((movie) => (
+              <GenreItem
+                key={movie.id}
+                movie={movie}
+                selected={this.state.preview === movie.id}
+                onMouseEnter={() => this.setState({ preview: movie.id })}
+                onMouseLeave={() => this.setState({ preview: null })}
+                deleteListMovie={this.props.deleteListMovie}
+                addToList={this.props.addToList}
+                currentUser={this.props.currentUser}
+                lists={this.props.lists}
+              />
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
 
-    componentDidUpdate(prevState){
-        const query = this.props.query; 
-        if(prevState.query != query){
-            this.debounce(()=>{
-                if(query.length > 0)
-                    this.props.searchByName(query)
-            }, 2000)();
-        }
-    }
-
-    debounce(func, delay){ 
-        return ()=> { 
-            if(this.debounceTimer)
-                clearTimeout(this.debounceTimer) 
-            this.debounceTimer  = setTimeout(func, delay) 
-        } 
-    }
-
-    makeSlices(movies, elementToSkip = 6){
-        if(Object.values(movies).length ==0){
-            return (
-                <>
-                </>
-            )
-        }
-        return (
-            <>
-            <div className='genre'>
-                <div className="search-items">
-                    {movies.map((movie) => (
-                    <GenreItem
-                        key={movie.id}
-                        movie={movie}
-                        selected={this.state.preview === movie.id}
-                        onMouseEnter={() => this.setState({ preview: movie.id })}
-                        onMouseLeave={() => this.setState({ preview: null })}
-                        deleteListMovie={this.props.deleteListMovie}
-                        addToList={this.props.addToList}
-                        currentUser={this.props.currentUser}
-                        lists={this.props.lists}
-                    />
-                    ))}
-                </div>
-            </div>
-            <>
-                {
-                    this.makeSlices(movies.slice(elementToSkip), elementToSkip)
-                }
-            </>
-            </>
-        )
-    }
-
-    render() {
-        return (
+  render() {
+    return (
+      <div>
+        <div className="search-results">
+          {this.props.movies.length > 0 ? (
+            this.showSearch(this.props.movies)
+          ) : (
             <div>
-                <div className="search-results">
-                    {
-                      this.props.movies.length > 0 ? this.makeSlices(this.props.movies) :
-                      <div>
-                          <p className="not-found">
-                            {`Your search for "${this.props.query}" did not have any matches. Suggestions:`} </p>
-                            <ul className="listing">
-                                <li>Try different keywords</li>
-                                <li>Try using a movie title</li>
-                            </ul>
-                      </div> 
-                    }
-                </div>
+              <p className="not-found">
+                {`Your search for "${this.props.query}" did not have any matches. Suggestions:`}{' '}
+              </p>
+              <ul className="listing">
+                <li>Try different keywords</li>
+                <li>Try using a movie title</li>
+              </ul>
             </div>
-        );
-    }
-
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
